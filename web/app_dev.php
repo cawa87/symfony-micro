e62a68b99ae07db502880d2afae5a9d0c71bf608
Ramiro Anacona Meneses <anacona16@gmail.com>
2015-11-19 16:23:38 -0500

    remove empty lines

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,7 +3,6 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
