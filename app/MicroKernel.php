3d2897a221b0c4938768fc92dc52749f081f7f03
Aleksandr Zamiatin <cawa123@mail.ru>
2015-11-20 16:41:14 +0200

    create response directly from twig content

--- app/MicroKernel.php
+++ app/MicroKernel.php
@@ -24,6 +24,28 @@ class MicroKernel extends Kernel
         return $bundles;
     }
 
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__) . '/var/cache/' . $this->environment;
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__) . '/var/logs';
+    }
+
+    public function indexAction()
+    {
+        return new \Symfony\Component\HttpFoundation\Response(
+            $this->container->get('twig')->render('index.html.twig')
+        );
+    }
+
     protected function configureRoutes(RouteCollectionBuilder $routes)
     {
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
@@ -36,26 +58,6 @@ class MicroKernel extends Kernel
 
     protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
-    }
-
-    public function getRootDir()
-    {
-        return __DIR__;
-    }
-
-    public function getCacheDir()
-    {
-        return dirname(__DIR__).'/var/cache/'.$this->environment;
-    }
-
-    public function getLogDir()
-    {
-        return dirname(__DIR__).'/var/logs';
-    }
-
-    public function indexAction()
-    {
-        return $this->container->get('templating')->renderResponse('index.html.twig');
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }
